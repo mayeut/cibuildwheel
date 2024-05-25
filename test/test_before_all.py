f97f3edf5c7b1d68c6d0a1d8e9dc59c4c85f44a1
mayeut <mayeut@users.noreply.github.com>
2024-05-25 19:02:48 +0200

    chore(tests): limit number of builds in test_before_all

--- test/test_before_all.py
+++ test/test_before_all.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import subprocess
+import sys
 import textwrap
 
 import pytest
@@ -14,9 +15,13 @@ project_with_before_build_asserts = test_projects.new_c_project(
         # is the same one as is currently running.
         with open("text_info.txt") as f:
             stored_text = f.read()
-
         print("## stored text: " + stored_text)
-        assert stored_text == "sample text 123"
+        parts = stored_text.split("+")
+        assert len(parts) == 3
+        assert parts[0] == "cpython"
+        if sys.implementation.name == "cpython":
+            assert sys.hexversion != int(parts[1])
+        assert parts[2] == "sample text 123"
         """
     )
 )
@@ -29,22 +34,38 @@ def test(tmp_path):
     with (project_dir / "text_info.txt").open(mode="w") as ff:
         print("dummy text", file=ff)
 
+    # we want to limit the number of builds to speed-up tests
+    builds = ["pp310"]  # always one version of PyPy
+    if utils.platform == "linux":
+        # Linux uses cp38 as a global python
+        builds.append("cp312")
+    else:
+        # use a CPython version that's not running cibuildwheel
+        for minor in range(12, 8, -1):
+            if (3, minor) != sys.version_info[:2]:
+                builds.append(f"cp3{minor}")
+                break
+    assert len(builds) == 2
+
     # build the wheels
-    before_all_command = '''python -c "import os;open('{project}/text_info.txt', 'w').write('sample text '+os.environ.get('TEST_VAL', ''))"'''
+    before_all_command = '''python -ISc "import os, sys;open('{project}/text_info.txt', 'w').write(f'{sys.implementation.name}+{sys.hexversion}+sample text '+os.environ.get('TEST_VAL', ''))"'''
     actual_wheels = utils.cibuildwheel_run(
         project_dir,
         add_env={
+            "CIBW_BUILD": " ".join(f"{build}-*" for build in builds),
             # write python version information to a temporary file, this is
             # checked in setup.py
             "CIBW_BEFORE_ALL": before_all_command,
-            "CIBW_BEFORE_ALL_LINUX": f'{before_all_command} && python -c "import sys; assert sys.version_info >= (3, 6)"',
+            "CIBW_BEFORE_ALL_LINUX": f'{before_all_command} && python -ISc "import sys; assert sys.version_info >= (3, 6)"',
             "CIBW_ENVIRONMENT": "TEST_VAL='123'",
         },
     )
 
     # also check that we got the right wheels
     (project_dir / "text_info.txt").unlink()
-    expected_wheels = utils.expected_wheels("spam", "0.1.0")
+    expected_wheels = [
+        w for w in utils.expected_wheels("spam", "0.1.0") if any(build in w for build in builds)
+    ]
     assert set(actual_wheels) == set(expected_wheels)
 
 
@@ -72,7 +93,8 @@ def test_cwd(tmp_path):
             "CIBW_BEFORE_ALL": f'''python -c "import os; assert os.getcwd() == {str(project_dir)!r}"''',
             "CIBW_BEFORE_ALL_LINUX": '''python -c "import os; assert os.getcwd() == '/project'"''',
         },
+        single_python=True,
     )
 
-    expected_wheels = utils.expected_wheels("spam", "0.1.0")
+    expected_wheels = utils.expected_wheels("spam", "0.1.0", single_python=True)
     assert set(actual_wheels) == set(expected_wheels)
