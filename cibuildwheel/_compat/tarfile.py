3600f09c074362ed679ff686e1c0cbc5d3ec5b72
mayeut <mayeut@users.noreply.github.com>
2026-06-28 07:28:25 +0200

    drop-cli-313

--- cibuildwheel/_compat/tarfile.py
+++ cibuildwheel/_compat/tarfile.py
@@ -2,25 +2,13 @@
 
 from __future__ import annotations
 
-import sys
 import tarfile
 
 TYPE_CHECKING = False
 if TYPE_CHECKING:
     from pathlib import Path
 
-    TarFile = tarfile.TarFile
-
-# Per https://peps.python.org/pep-0706/, the "data" filter will become
-# the default in Python 3.14. The first series of releases with the filter
-# had a broken filter that could not process symlinks correctly.
-elif sys.version_info < (3, 14):
-
-    class TarFile(tarfile.TarFile):  # pragma: no cover
-        extraction_filter = staticmethod(tarfile.data_filter)
-
-else:
-    TarFile = tarfile.TarFile  # pragma: no cover
+TarFile = tarfile.TarFile  # pragma: no cover
 
 
 def safe_extractall(tar: tarfile.TarFile, path: Path) -> None:  # pragma: no cover
